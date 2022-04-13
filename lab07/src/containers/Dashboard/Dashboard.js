91490a0f61dfb52719a1dfa08f09dec4e82278df
ghideibahta <90098254+ghideibahta@users.noreply.github.com>
2022-04-13 12:34:04 -0700

    lab7

---
+++ lab07/src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Post from "../../components/Post/Post";
+import Posts from "../../containers/Postss/Postss";
+//import "./DashBoard.css";
+
+
+const DashBoard = () => {
+    const [title, setTitle] = useState("");
+    const [posts, setPosts] = useState(
+        [
+            { id: 111, title: 'Happiness', author: 'John' },
+            { id: 112, title: 'MIU', author: 'Dean' },
+            { id: 113, title: 'EnjoyLife', author: 'Jasmine' }
+        ]);
+    const titleHandler = (title) => {
+        posts[0].title = title;
+        setPosts([...posts]);
+    }
+
+    return (
+        <div>
+            <Posts posts={posts} />
+            <fieldset className="form">
+                <input
+                    type="text"
+                    id="newTitle"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="Enter new title">
+                </input>
+                <br /><br />
+                <button onClick={() => titleHandler(title)}>Change Title</button>
+            </fieldset>
+        </div>
+
+);
+}
+export default DashBoard;
